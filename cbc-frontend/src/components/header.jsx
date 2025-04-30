ff55ee507e811dddca49f3f9905a8f1153b1de3b
KalpaniKapuge <kalpanikapuge1020@gmail.com>
2025-04-30 10:41:00 +0530

    Initializing the project✏️

---
+++ cbc-frontend/src/components/header.jsx
@@ -0,0 +1,15 @@
+import UserData from "../../../../CBC-batch-five-frontend-project test/cbc-frontend/src/components/userData";
+
+export default function Header(){
+    return(
+        <>
+            <div>
+                <h1>Crystal Beauty Salon</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem, 
+                    ratione impedit architecto ea consequuntur temporibus eveniet vel perferendis quae nemo?
+                </p>
+                <UserData/>
+            </div>
+        </>
+    )
+}
